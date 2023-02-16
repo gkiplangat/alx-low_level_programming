855d87b498e4231acfa130f68b65d297299cd872
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-16 09:41:57 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/** 
+ * main - prints "programming is like a multilingual 
+ * puzzle, followed by a newline."
+ *return 0.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like a building a mutilingual puzzle");
+	return(0);
+}
