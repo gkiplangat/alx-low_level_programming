223046499d1327c0027237d29f66047a0d523542
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-16 09:52:55 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
-
-/** 
- * main - prints "programming is like a multilingual 
- * puzzle, followed by a newline."
- *return 0.
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
