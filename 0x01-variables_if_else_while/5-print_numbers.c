bc7b4e5d160429f0c11d1dcd96884f8695f08dfc
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-17 13:18:20 -0800

    numbers between 0 and 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf("%d", num);
+num += 1;
+}
+printf("\n");
+return (0);
+}
