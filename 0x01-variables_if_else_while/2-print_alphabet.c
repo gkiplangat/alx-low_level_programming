a86aa873cea21f621d88efe00b3cd881ce44b892
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-17 12:47:24 -0800

    lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main -Entry point 
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+putchar(l);
+lowerCase += 1;
+}
+putchar('\n');
+return (0);
+}
