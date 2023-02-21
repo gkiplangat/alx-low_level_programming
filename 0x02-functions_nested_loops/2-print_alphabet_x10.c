adb00165bdbee973e9d770acf702014e6e50e4ec
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-21 10:11:30 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,19 @@
-#include
+#include "main.h"
+
+/**
+ * main - print_alphabet_x10
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+
+char ch;
+int i;
+
+for (i = 0; i < 10; i++)
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
