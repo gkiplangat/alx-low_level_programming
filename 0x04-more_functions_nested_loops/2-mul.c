ba9c1f758bc170f968ef9462903f9ae135b79659
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-23 01:47:37 -0800

    Multiply two intergers and print the result

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,17 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: - The first integer
+ * @b: - The second integer
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+int result = a * b;
+printf("%d\n", result);
+return (0);
+}
+
