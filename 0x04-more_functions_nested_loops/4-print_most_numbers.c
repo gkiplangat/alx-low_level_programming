3e9fe872cfbd4021f42738acafb4fed6a5c18ae0
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-23 10:28:48 -0800

    output

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,10 @@ void print_most_numbers(void)
 {
 char c;
 
-for (c = '0'; c <= 9; c++)
-{
-if (!(c == '2' || c == '4'))
-_putchar(c);
-}
-_putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
+	}
+	_putchar('\n');
 }
