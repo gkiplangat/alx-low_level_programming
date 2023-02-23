d02fe9b7ed01857f03127b69a95e8607b8397f5c
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-23 00:19:16 -0800

    execuable file created

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * main -Entry point
+ * _isdigit tests an integer value.
+ * Return : 1 for digitotherwise 0
+ */
+
+int _isdigit(int c)
+
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
