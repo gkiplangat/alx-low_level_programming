18b6e720f7563916efa992ab6529c75824ec2696
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-23 04:22:26 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,12 @@
 
 /**
  * _isupper - Checks for lowercase character
- * @c: The character to be checked
+ * @c:The character to be checked
  * Return: 1 for uppercase character or 0 for anything else
  */
 
 int _isupper(int c)
+
 {
 if (c >= 65 && c <= 97)
 {
