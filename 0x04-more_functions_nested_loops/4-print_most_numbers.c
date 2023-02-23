f1de67531cad6fa975178dd8316824356b853610
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-23 10:09:02 -0800

    print_most_numbers.c file updatesd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,3 +1,17 @@
 #include "main.h"
 
 /**
+ * print_most_numbers -prints 01356789
+ * Return:void
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+	for (c = '0'; c <= 9; c++)
+		if(c != 2 && c != 4)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+}
