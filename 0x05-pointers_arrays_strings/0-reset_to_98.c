2618de4bf204b98412fab12af215a2ec8fc1af04
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-27 08:14:48 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Update the value of the pointer it points
+ * @n:-the value to be changed
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+
+
+*n = 98;
+}
