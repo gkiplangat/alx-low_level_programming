ed6eb1e7309427ac13a51b274f6b6264d1861ecd
gkiplangat <gideonkiplangat4@gmail.com>
2023-02-27 08:28:39 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int -swaps the value of the pointer it points to
+ * @a:-first value
+ * @b:-second value
+ * Return:Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
