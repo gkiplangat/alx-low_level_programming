4f7e15982d4cd7dc8c49dc4ea9fc860e1624cfc6
gkiplangat <gideonkiplangat4@gmail.com>
2023-03-02 05:58:00 -0800

    1-main.c file created

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  *
  *
  */
 
+char *_strcat(char *dest, char *src)
+{
 
+	dest = "Hello ";
+	src = "World";
+	
+	strcat(dest, src);
+	printf("strcat(dest,src): %s\n", dest);
+	return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,2 +1,2 @@
-int _putchar(char c)i;
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
