2bb9f4a10c8050d9dfd52076dbeaea9588426379
gkiplangat <gideonkiplangat4@gmail.com>
2023-03-16 10:31:16 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 
 /**
@@ -11,8 +13,7 @@
 void *malloc_checked(unsigned int b)
 {
    void *i;
-
-
+   
    i = malloc(b);
    if (i == NULL)
        exit(98);
