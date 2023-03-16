18d67bb7ed254e5d00fe1e7bdf8c2b0512fb96cc
gkiplangat <gideonkiplangat4@gmail.com>
2023-03-16 08:40:25 -0700

    main.h updated

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
