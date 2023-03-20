2f781454c000cccbed0cedb04881ae90259401f2
gkiplangat <gideonkiplangat4@gmail.com>
2023-03-20 05:31:44 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ *struct dog - information about dogs
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/**
+ *dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
