aa13c913b0ca2ac9ccbad3aa7bf638846e457bef
gkiplangat <gideonkiplangat4@gmail.com>
2023-03-30 11:33:44 -0700

    fixing betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,14 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+	size_t n = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
-
-
