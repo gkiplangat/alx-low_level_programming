519858fa214025aac1d3b572dd1a915f4f95b289
gkiplangat <gideonkiplangat4@gmail.com>
2023-03-30 11:43:05 -0700

    fixing betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,14 +7,13 @@
  */
 void free_list(list_t *head)
 {
- list_t *temp;
+	list_t *temp;
 
- while (head)
- {
- temp = head->next;
- free(head->str);
- free(head);
- head = temp;
- }
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
-
