219f3ba88f44624c6e37803d5cd4e7dd229e9f98
gkiplangat <gideonkiplangat4@gmail.com>
2023-04-08 13:36:20 -0700

    git_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,12 +10,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 	return (-1);
 	}
-
-	unsigned long int mask;
 
 	mask = 1ul << index;
 
@@ -25,7 +25,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	}
 	else
 	{
-	return (0);
+	return (-1);
 	}
 }
 
