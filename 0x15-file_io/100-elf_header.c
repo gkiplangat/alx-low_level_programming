3f4041248ac522137d0a7ff902fbe2b79e037f19
gkiplangat <gideonkiplangat4@gmail.com>
2023-04-10 15:51:04 -0700

    advance

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,35 @@
+/**
+ * print_type - Prints the type of an ELF header.
+ * @e_type: The ELF type.
+ * @e_ident: A pointer to an array containing the ELF class.
+ */
+void print_type(unsigned int e_type, unsigned char *e_ident)
+{
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+	e_type >>= 8;
+
+	printf("  Type:                              ");
+
+	switch (e_type)
+	{
+	case ET_NONE:
+	printf("NONE (None)\n");
+	break;
+	case ET_REL:
+	printf("REL (Relocatable file)\n");
+	break;
+	case ET_EXEC:
+	printf("EXEC (Executable file)\n");
+	break;
+	case ET_DYN:
+	printf("DYN (Shared object file)\n");
+	break;
+	case ET_CORE:
+	printf("CORE (Core file)\n");
+	break;
+	default:
+	printf("<unknown: %x>\n", e_type);
+	break;
+	}
+}
+
