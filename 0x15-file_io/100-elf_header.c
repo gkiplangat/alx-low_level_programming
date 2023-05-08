b74f035398ed366109160c9262c8431c22a559d5
gkiplangat <gideonkiplangat4@gmail.com>
2023-05-08 07:22:13 -0700

    updated elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -306,7 +306,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
     print_osabi(header->e_ident);
     print_abi(header->e_ident);
     print_type(header->e_type, header->e_ident);
-    print_entry(header->e_entry, header->e_ident);
+    print_entry(header->e_entry, header->e_ident1);
 
     free(header);
     close_elf(o);
