b6af801a1bfac2d0fbe8d2aa7152e64779b51cff
gkiplangat <gideonkiplangat4@gmail.com>
2023-06-19 10:24:00 -0700

    code injection

---
+++ 0x18-dynamic_libraries/test.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int printf(const char *format, ...)
+{
+	write(1, "9 8 10 24 75 -9\n", 17);
+	write(1, "Congratulations, you win the jackpot!\n", 38);
+	exit(EXIT_SUCCESS);
+}
