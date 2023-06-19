c826e2ead8f546e0543d8d4ae051e578f26c4b5c
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2023-06-19 20:42:15 +0300

    Update 101-make_me_win.sh
    update the file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/debugulize/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/gkiplangat/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
