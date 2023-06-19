3a0b96cd3bbaa8581b31c43ce7e64b0bce8a5b9a
gkiplangat <gkiplangat>
2023-06-19 21:12:03 +0300

    fix:output

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/gkiplangat/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/gkiplangat/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD=/tmp/libmask.so
