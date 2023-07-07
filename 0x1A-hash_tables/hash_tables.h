f627c13283c599ec58a22e0e0a29105a11a9cb05
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-07 04:24:37 -0700

    fix:correct output

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,13 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
 /**
  * struct hash_node_s - Node of a hash table
  *
