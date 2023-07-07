1dbb80f28ec3eead36295beae22b6f4ecdf43e13
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-07 04:49:26 -0700

    feat:2-key_index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - hash table array index position.
+ * @key: The key to be hashed.
+ * @size: Size of the hash table array.
+ *
+ * Return: The index of the key on the hash table array.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hsh, index;
+
+	hash = hash_djb2(key);
+	index = hsh % size;
+
+	return (index);
+}
