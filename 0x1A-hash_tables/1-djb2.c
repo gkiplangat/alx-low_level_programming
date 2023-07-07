455b7952c69fd1e1c06012fd1876f5a272e23722
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-07 03:04:54 -0700

    feat:1-djb2.c fixed correct output

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,18 @@
+/**
+ * hash_djb2 - implementing djb2 hash alogarithim
+ * @str: -string used to generate hash value
+ *
+ * Return: hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
