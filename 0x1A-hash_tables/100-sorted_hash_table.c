7b752a91a244c2a68ae705c8ca462463830f51a2
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-07 03:40:12 -0700

    100-sorted_hash table

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,63 @@
+#include "hash_tables.h"
+
+/**
+ * shash_table_create - create table
+ * @size: - size of the table
+ *
+ * Return: table size
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+}
+/**
+ * shash_table_set - set values into the table
+ * @key: - hash key
+ * @value: -value to be fed into table
+ *
+ * return:no of values
+ */
+
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+}
+/**
+ * shash_table_get -get characters in a table
+ * @ht: -head
+ * @key: -hash key
+ *
+ * Return: char
+ */
+
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+}
+/**
+ * shash_table_print_rev - print table
+ * @ht: -head
+ *
+ * Return: void
+ */
+
+void shash_table_print(const shash_table_t *ht)
+{
+}
+
+/**
+ * shash_table_print_rev - print table
+ * @ht: head
+ *
+ * Return: void
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+}
+/**
+ * shash_table_delete - delete table
+ * @ht: head
+ *
+ * Return: void
+ */
+
+void shash_table_delete(shash_table_t *ht)
+{
+}
