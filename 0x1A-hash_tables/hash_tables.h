80d0e4cb3c417f48ca6de1fc8a39f120b5614761
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-07 04:41:40 -0700

    feat:updated the hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,12 +1,10 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
+#ifndef _HASH_TABLES_
+#define _HASH_TABLES_
 
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-
+#include <string.h>
+#include <unistd.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -18,9 +16,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+		 char *key;
+		 char *value;
+		 struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -33,11 +31,11 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+		 unsigned long int size;
+		 hash_node_t **array;
 } hash_table_t;
 
-/*PHP structures*/
+/* >>>>>>>>>>>>>>>>>>>>>>>>>> PHP Structs >>>>>>>>>>>>>>>>>>>>>>>>> */
 /**
  * struct shash_node_s - Node of a sorted hash table
  *
@@ -50,11 +48,11 @@ typedef struct hash_table_s
  */
 typedef struct shash_node_s
 {
-     char *key;
-     char *value;
-     struct shash_node_s *next;
-     struct shash_node_s *sprev;
-     struct shash_node_s *snext;
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
 } shash_node_t;
 
 /**
@@ -69,13 +67,13 @@ typedef struct shash_node_s
  */
 typedef struct shash_table_s
 {
-     unsigned long int size;
-     shash_node_t **array;
-     shash_node_t *shead;
-     shash_node_t *stail;
+	 unsigned long int size;
+	 shash_node_t **array;
+	 shash_node_t *shead;
+	 shash_node_t *stail;
 } shash_table_t;
+/* >>>>>>>>>>>>>>>>>>>>>>>>>> End PHP Structs >>>>>>>>>>>>>>>>>>>>>>>>> */
 
-/*Functions declarations*/
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -83,7 +81,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-/*PHP prototypes*/
+
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
@@ -91,5 +89,4 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-
-#endif
+#endif /* _HASH_TABLES_ */
