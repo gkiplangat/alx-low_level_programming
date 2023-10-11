e8c8192feee8d01d080c84c575ec0f6fe5bdab2f
gkiplangat <gideonkiplangat4@gmail.com>
2023-10-11 13:39:56 -0700

    feat:added linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+* linear_search.
+ * @array: Integer array to be searched
+ * @size: The size of the array
+ * @value: The value to search for
+ *
+ * Return: The first index where the value is located or -1 if not found.
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t u;
+
+	if (array == NULL)
+		return (-1);
+
+	for (u = 0; u < size; u++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", u, array[u]);
+		if (array[u] == value)
+		{
+			return (u);
+		}
+	}
+
+	return (-1);
+}
